0e8832427eef52a19ee8096c7067cc0b63c2ce2d
geodan89 <geodan89@gmail.com>
2020-04-17 20:58:40 +0300

    Final commit, app ready.

---
+++ src/main/java/geo/springframework/joke/springjokeapp/controllers/JokeController.java
@@ -0,0 +1,26 @@
+package geo.springframework.joke.springjokeapp.controllers;
+
+import geo.springframework.joke.springjokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
---
+++ src/main/java/geo/springframework/joke/springjokeapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package geo.springframework.joke.springjokeapp.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
