a9f6088519c6d9d2915dceb38bbda980ae55023a
geodan89 <geodan89@gmail.com>
2020-04-17 19:35:06 +0300

    First commit

---
+++ src/test/java/geo/springframework/joke/springjokeapp/SpringJokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package geo.springframework.joke.springjokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
